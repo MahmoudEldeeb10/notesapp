64afa559555ddf554693d28d80aec1034b907154
MahmoudEldeeb10 <nett445g@gmail.com>
2025-07-13 23:09:28 +0300

    edit note view

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -6,6 +6,8 @@ class EditNoteView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: EditNoteViewBody());
+    return const Scaffold(body: EditNoteViewBody(),
+    
+    );
   }
 }
--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -13,6 +14,10 @@ class EditNoteViewBody extends StatelessWidget {
           children: [
             SizedBox(height: 20),
             CustomAppBar(title: 'Edit Note', icon: Icons.check),
+            SizedBox(height: 50),
+            CustomTextField(hintText: 'Title'),
+            SizedBox(height: 16),
+            CustomTextField(hintText: 'content', maxLines: 5),
           ],
         ),
       ),
