5dbafafee48fe67b523dd3ec91c26dfa0cd23d8c
MahmoudEldeeb10 <nett445g@gmail.com>
2025-08-02 16:54:47 +0300

    delete note

--- lib/cubits/notes cubit/notes_cubit.dart
+++ lib/cubits/notes cubit/notes_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive/hive.dart';
+import 'package:meta/meta.dart';
 import 'package:notes_app/constsnts.dart';
 import 'package:notes_app/models/note_model.dart';
 
@@ -8,10 +8,12 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-  List<NoteModel> notes = [];
-  fetchAllNotes() async {
+
+  List<NoteModel>? notes;
+  fetchAllNotes() {
     var notesBox = Hive.box<NoteModel>(kNotesBox);
 
-    List<NoteModel> notes = notesBox.values.toList();
+    notes = notesBox.values.toList();
+    emit(NotesSuccess());
   }
 }
